f5825d2bf0ba6d66d77a9c1a2fc407b056e25344
Lorris Lehuede <lehuedelorris@gmail.com>
2021-11-25 21:57:11 +0100

    first commit

--- server.ts
+++ server.ts
@@ -1,19 +1,76 @@
+// @ts-nocheck
+const fs = require("fs");
+const path = require("path");
 import express, { Response, Request, NextFunction } from "express";
 import cors from "cors";
 const asyncHandler = require("express-async-handler");
 require("dotenv").config();
 
-import controller from "./src/controllers/controller";
+// import controller from "./src/controllers/controller";
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
+// Controller
+// @ts-nocheck
+
+const controller = {
+  getImages: async (req, res) => {
+    var walk = function (dir, done) {
+      var results = [];
+      fs.readdir(dir, function (err, list) {
+        if (err) return done(err);
+        var i = 0;
+        (function next() {
+          var file = list[i++];
+          if (!file) return done(null, results);
+          file = path.resolve(dir, file);
+          fs.stat(file, function (err, stat) {
+            if (stat && stat.isDirectory()) {
+              walk(file, function (err, res) {
+                results = results.concat(res);
+                next();
+              });
+            } else {
+              results.push(file);
+              next();
+            }
+          });
+        })();
+      });
+    };
+
+    const startingFolder =
+      "/home/grubux/Documents/portfolio/laCasa/frontend/public/images";
+
+    const customResponse = { imagesPaths: 0, pathStyled: 1 };
+
+    await walk(startingFolder, function (err, results) {
+      if (err) throw err;
+
+      var re = new RegExp("/(images/.*)/[^/]+$");
+
+      const pathStyled = results.map((result) => {
+        console.log("regex: ", result.match(re)[0]);
+        return result.match(re)[0];
+      });
+
+      console.log("pathStyled: ", pathStyled);
+
+      console.log(results);
+
+      customResponse.imagesPaths = results;
+      customResponse.pathStyled = pathStyled;
+      res.json(customResponse);
+    });
+  },
+};
 
 // Routes
 
-app.post("/api/v1", asyncHandler(controller.count));
+app.get("/api", asyncHandler(controller.getImages));
 
 app.get("*", (_, res) => {
   res.status(404);
--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -1,13 +1,49 @@
-import { Response, Request } from "express";
-export interface IInterface {
-  articleName: string;
-  price: number;
-  quantity: number;
-}
+// @ts-nocheck
+
+const fs = require("fs");
+const path = require("path");
 
 const controller = {
-  count: async (req: Request, res: Response): Promise<void> => {
-    res.send("sent");
+  getImages: async (req, res) => {
+    var walk = function (dir, done) {
+      var results = [];
+      fs.readdir(dir, function (err, list) {
+        if (err) return done(err);
+        var i = 0;
+        (function next() {
+          var file = list[i++];
+          if (!file) return done(null, results);
+          file = path.resolve(dir, file);
+          fs.stat(file, function (err, stat) {
+            if (stat && stat.isDirectory()) {
+              walk(file, function (err, res) {
+                results = results.concat(res);
+                next();
+              });
+            } else {
+              results.push(file);
+              next();
+            }
+          });
+        })();
+      });
+    };
+
+    const startingFolder = "";
+
+    walk(startingFolder, function (err, results) {
+      if (err) throw err;
+
+      var re = new RegExp("/(images/.*)/[^/]+$");
+
+      const pathStyled = results.map((result) => {
+        console.log("regex: ", result.match(re)[0]);
+      });
+
+      console.log(results);
+    });
+
+    // res.JSON({ imagesPaths: results, pathStyled: pathStyled });
   },
 };
 
